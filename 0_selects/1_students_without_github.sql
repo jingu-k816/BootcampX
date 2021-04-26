594286c66126c7210fdbd75dcaa635ed119d98f8
Jingu Kang <jinguk0816@gmail.com>
2021-04-26 14:07:34 -0400

    exercise on various types of queries

---
+++ 0_selects/1_students_without_github.sql
@@ -0,0 +1,9 @@
+SELECT
+  id, name, email, cohort_id
+FROM
+  students
+WHERE
+  github IS NULL
+ORDER BY
+  cohort_id
+;
---
+++ 1_queries/various_queries.sql
@@ -0,0 +1,52 @@
+-- SELECT
+--  id, name
+-- FROM
+--   students
+-- WHERE 
+--   cohort_id = 1
+-- ORDER BY
+--   name;
+
+-- SELECT
+--   COUNT(*)
+-- FROM
+--   students
+-- WHERE
+--   cohort_id IN (1,2,3)
+-- ;
+
+-- SELECT 
+--   name, id, cohort_id
+-- FROM
+--   students
+-- WHERE
+--   phone IS NULL
+--   OR 
+--   email IS NULL;
+
+-- SELECT
+--   name, id, email, cohort_id
+-- FROM
+--   students
+-- WHERE
+--   email NOT LIKE '%gmail.com'
+--   AND
+--   phone IS NULL;
+
+-- SELECT 
+--   name, id, cohort_id
+-- FROM
+--   students
+-- WHERE
+--   end_date IS NULL
+-- ORDER BY
+--   cohort_id;
+
+SELECT
+  name, email, phone
+FROM
+  students
+WHERE 
+  github IS NULL
+  AND
+  end_date IS NOT NULL;
